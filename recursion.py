80307ad3a0353c3271ef2f26ca607b744e69f8d6
DableBlock <1255887634@qq.com>
2018-04-16 14:51:23 +0800

    递归 斐波那契

--- recursion.py
+++ recursion.py
@@ -26,3 +26,32 @@ def getItems(l):
         else:
             print(i)
 getItems(s)
+#
+# 斐波那契数列
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return (fibonacci(n- 1)+ fibonacci(n- 2))
+fibonacci(10)
+
+
+# def recur_fibo(n):
+#     """递归函数
+#     输出斐波那契数列"""
+#     if n <= 1:
+#         return n
+#     else:
+#         return (recur_fibo(n - 1) + recur_fibo(n - 2))
+#
+#
+# # 获取用户输入
+# nterms = int(input("您要输出几项? "))
+#
+# # 检查输入的数字是否正确
+# if nterms <= 0:
+#     print("输入正数")
+# else:
+#     print("斐波那契数列:")
+#     for i in range(nterms):
+#         print(recur_fibo(i))
